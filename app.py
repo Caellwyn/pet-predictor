76253c7b244371f4083f68b69da25d544af9cb72
Josh Johnson <caellwyn@gmail.com>
2023-02-17 09:21:16 -0800

    add model to cache

--- app.py
+++ app.py
@@ -1,33 +1,60 @@
 import streamlit as st
 from src import PetModel
+from time import time
 
-   
-#Create a header for the webpage
-st.title('Is That a Dog or a Cat?')
+print('starting over')
+@st.cache(allow_output_mutation=True)
+def load_model():
+    return PetModel()
 
-#create an upload widget to receive an image file.  
-#Give it a name and define allowed file types
-image = st.file_uploader("Upload Your Pet!", type=["png","jpeg","jpg"])
+def make_prediction(model, img):
+    return model.predict_pet(img)
 
-#make a prediction and return the results on a new text line
-if image:
-    print('Downloaded Image!')
-    result = st.empty()
-    st.image(image)
-    result.write('Inspecting Image...')
-    
-    model = PetModel()
-    response = model.predict_pet(image)
-    print('made a prediction')
-    result.write(response)
+def explain_prediction(model):
+    return model.explain_prediction()
 
-    #show an image with a mask showing model activations
-    st.header('If you wait a minute or two, I can explain my prediction')
-    result2 = st.empty()
-    result2.write('Please wait while I gather my thoughts...')
-    explanation = model.explain_prediction()
-    print('created an explanation image')
-    result2.write('This is what I noticed:\nGreen makes me think this is a dog.\nRed makes me think this is a cat')
-    st.image(explanation, use_column_width='always')
-    del explanation
-    del image
+
+if __name__ == "__main__":
+
+    #Create a header for the webpage
+    st.title('Is That a Dog or a Cat?')
+
+    #create an upload widget to receive an image file.  
+    image = st.file_uploader("Upload Your Pet!", type=["png","jpeg","jpg"])
+
+
+    #make a prediction and return the results on a new text line
+    if image:
+        print('Downloaded Image!')
+        st.image(image)
+        result = st.empty()
+        result.write('Inspecting Image...')
+
+        #start loading model
+        start = time()
+        model = load_model()
+        print(f'model loaded in {time() - start}')
+
+        # predict image
+        start = time()
+        response = make_prediction(model, image)
+        print(f'model predicted in {time() - start}')
+        print('made a prediction')
+        result.write(response)
+
+        #show an image with a mask showing model activations
+        st.header('If you wait a minute or two, I can explain my prediction')
+        result2 = st.empty()
+        result2.write('Please wait while I gather my thoughts...')
+
+        start = time()
+        explanation = explain_prediction(model)
+        print(f'model explained predictions in {time() - start}')
+        print('created an explanation image')
+        result2.write('This is what I noticed:\nGreen makes me think this is a dog.\nRed makes me think this is a cat')
+        st.image(explanation, use_column_width='always')
+        del explanation
+        del image
+        del model
+
+
